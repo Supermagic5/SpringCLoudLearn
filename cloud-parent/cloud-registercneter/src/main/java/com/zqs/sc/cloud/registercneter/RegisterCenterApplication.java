dad35c550a7f96aa8a98a40ed4612c65d71cc886
Supermagic5 <804858758@qq.com>
2018-06-23 16:14:21 +0800

    测试

--- cloud-parent/cloud-registercneter/src/main/java/com/zqs/sc/cloud/registercneter/RegisterCenterApplication.java
+++ cloud-parent/cloud-registercneter/src/main/java/com/zqs/sc/cloud/registercneter/RegisterCenterApplication.java
@@ -3,7 +3,11 @@ package com.zqs.sc.cloud.registercneter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+/**
+ * 注册中心
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RegisterCenterApplication {
