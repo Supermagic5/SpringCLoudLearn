d1023482e9bde1049b0344d04843fa90d04267c9
Supermagic5 <804858758@qq.com>
2018-06-23 17:15:25 +0800

    删除测试

--- cloud-parent/cloud-registercneter/src/main/java/com/zqs/sc/cloud/registercneter/RegisterCenterApplication.java
+++ cloud-parent/cloud-registercneter/src/main/java/com/zqs/sc/cloud/registercneter/RegisterCenterApplication.java
@@ -3,12 +3,7 @@ package com.zqs.sc.cloud.registercneter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-/**
- * 注册中心
- * 测试3
- * @author Administrator
- *
- */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RegisterCenterApplication {
